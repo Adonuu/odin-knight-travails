398bcda2ad3266e2ef4e22539b0c8626904a3a99
Adam Young <adamyoung1414@gmail.com>
2024-09-01 12:59:41 -0400

    added function for knight moves to figure out the least amount of moves using BFS

---
+++ knight.js
@@ -0,0 +1,45 @@
+export function knightMoves(initialPos, finalPos) {
+    const possibleMoves = [
+      //first group of moves
+      [1, 2],
+      [1, -2],
+      [-1, 2],
+      [-1, -2],
+      //second group of moves
+      [2, 1],
+      [2, -1],
+      [-2, 1],
+      [-2, -1],
+    ];
+  
+    function bfs(start, goal) {
+      const queue = [[start]];
+      const visited = new Set();
+  
+      visited.add(start.toString());
+      while (queue.length > 0) {
+        const path = queue.shift();
+        const currentPos = path[path.length - 1];
+  
+        if (currentPos.toString() === goal.toString()) {
+          return path;
+        }
+        for (const move of possibleMoves) {
+          const nextPos = [currentPos[0] + move[0], currentPos[1] + move[1]];
+          if (nextPos[0] > -1 && nextPos[0] < 8 && nextPos[1] > -1 && nextPos[1] < 8 && !visited.has(nextPos.toString())) {
+            visited.add(nextPos.toString());
+            queue.push([...path, nextPos]);
+          }
+        }
+      }
+      return null;
+    }
+  
+    const result = bfs(initialPos, finalPos);
+    console.log(`Knight goes from ${initialPos} -> ${finalPos} in ${result.length - 1} moves, which are:`,);
+    console.log(result);
+    return result;
+  }
+
+  knightMoves([3,3],[4,3]);
+  knightMoves([0,0],[3,3]);
